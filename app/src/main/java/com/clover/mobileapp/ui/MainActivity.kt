fee5b9152ff3ba25d373eab758ae52f0cd95f4bd
RAVI <ravi20101990@gmail.com>
2021-11-14 21:16:00 +0530

    Fixed back button issue.

--- app/src/main/java/com/clover/mobileapp/ui/MainActivity.kt
+++ app/src/main/java/com/clover/mobileapp/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.clover.mobileapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 import com.clover.mobileapp.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,5 +11,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpToolBar()
+    }
+
+    private fun setUpToolBar() {
+        val actionBar: ActionBar? = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
